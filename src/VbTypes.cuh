e8d32d6579d33fb68abfe666daee2741e3b346d9
SolidShaker <kostianko_maksim@outlook.com>
2026-04-29 19:06:10 +0800

    Fix Variable

--- src/VbTypes.cuh
+++ src/VbTypes.cuh
@@ -19,23 +19,52 @@ namespace Vb
             Variable& operator=(const Variable&) = delete;
 
         public:
-            Variable(Variable&& other) noexcept;
+            Variable(Variable&& other) noexcept
+            {
+                data = other.data;
+                other.data = nullptr;
+            }
 
-            Variable& operator=(Variable&& other) noexcept;
+            Variable& operator=(Variable&& other) noexcept
+            {
+                if (this != &other)
+                {
+                    if (data) cudaFree(data);
+                    data = other.data;
+                    other.data = nullptr;
+                }
+                return *this;
+            }
 
         public:
             Variable() = default;
-            ~Variable();
+            ~Variable()
+            {
+                if (data)
+                    cudaFree(data);
+            }
 
         public:
             void 
-            Allocate(size_t amount);
+            Allocate(size_t amount)
+            {
+                if (data) cudaFree(data);
+
+                cudaMalloc((void**)&data, amount * sizeof(T));
+                cudaMemset(data, 0, amount*sizeof(T));
+            }
             
             void 
-            Write(const T *inp, size_t amount);
+            Write(const T *inp, size_t amount)
+            {
+                cudaMemcpy(data, inp, amount * sizeof(T), cudaMemcpyHostToDevice);
+            }
 
             void 
-            Read(T *out, size_t amount);
+            Read(T *out, size_t amount)
+            {
+                cudaMemcpy(out, data, amount * sizeof(T), cudaMemcpyDeviceToHost);
+            }
 
         public:
             T* Get() { return data; }
