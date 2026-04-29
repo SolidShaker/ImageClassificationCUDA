2063aeee77765c3e26c35089ea44b839b6eccefe
SolidShaker <kostianko_maksim@outlook.com>
2026-04-29 21:47:12 +0800

    Fix

--- src/main.cu
+++ src/main.cu
@@ -20,9 +20,9 @@ int main()
     int N = 2505;
     int K = 2502;
 
-    int pM= Vb::GetPadding(M, 4);
-    int pN= Vb::GetPadding(N, 4);
-    int pK= Vb::GetPadding(K, 4);
+    int pM= Vb::GetPadding(M, 32);
+    int pN= Vb::GetPadding(N, 32);
+    int pK= Vb::GetPadding(K, 32);
 
     size_t sizeA = M * K;
     size_t sizeB = K * N;
