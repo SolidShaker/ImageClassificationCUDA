13e2ef03f01f5fd3d52b74c4e2ad82dd084ec7c9
SolidShaker <kostianko_maksim@outlook.com>
2026-04-29 19:52:41 +0800

    Testing

--- src/main.cu
+++ src/main.cu
@@ -23,76 +23,115 @@ void cpuGEMM(const float* A, const float* B, float* C, int M, int N, int K)
     }
 }
 
+__global__ void test(float* C, int N)
+{
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < N)
+        C[i] = 1.0f;
+}
 
+void runTestKernel(float* dC, int N)
+{
+    int threads = 256;
+    int blocks = (N + threads - 1) / threads;
+
+    test<<<blocks, threads>>>(dC, N);
+
+    cudaDeviceSynchronize();
+}
 
 int main()
 {
-    Vb::Timer gpuClock;
-    int M = 2500;
-    int N = 2505;
-    int K = 2502;
+    int N = 1000;
 
-    int pM= Vb::GetPadding(M, 4);
-    int pN= Vb::GetPadding(N, 4);
-    int pK= Vb::GetPadding(K, 4);
+    float* hC = new float[N];
+    float* dC;
 
-    size_t sizeA = M * K;
-    size_t sizeB = K * N;
-    size_t sizeC = M * N;
+    cudaMalloc(&dC, N * sizeof(float));
 
-    float* hA = new float[sizeA];
-    float* hB = new float[sizeB];
-    float* hC = new float[sizeC];
+    runTestKernel(dC, N);
 
-    for (size_t i = 0; i < sizeA; i++) hA[i] = 1.0f;
-    for (size_t i = 0; i < sizeB; i++) hB[i] = 2.0f;
+    cudaMemcpy(hC, dC, N * sizeof(float), cudaMemcpyDeviceToHost);
 
-    Vb::Variable<float> dA;
-    Vb::Variable<float> dB;
-    Vb::Variable<float> dC;
-
-    dA.Allocate(pM * pK);
-    dB.Allocate(pK * pN);
-    dC.Allocate(pM * pN);
-
-    dA.Write(hA, sizeA);
-    dB.Write(hB, sizeB);
-
-    gpuClock.StartRecord();
-
-    Vb::GEMM(dA.Get(), dB.Get(), dC.Get(), M, N, K);
-    CUDA_CHECK("An error occured in GEMM");
-    cudaDeviceSynchronize();
-
-    gpuClock.StopRecord();
-
-    dC.Read(hC, sizeC);
-
-    std::cout << "GPU Time: " << gpuClock.GetTime() << "\n";
-    std::cout << "M_padded=" << M << " N_padded=" << N << " K_padded=" << K << "\n";
     for (int i = 0; i < 10; i++)
         std::cout << hC[i] << " ";
-    std::cout << "\n expected =" << 2.0f * K;
 
-    float* hC_cpu = new float[sizeC];
-    cpuGEMM(hA, hB, hC_cpu, M, N, K);
+    std::cout << std::endl;
 
-    float maxError = 0.0f;
-    int badCount = 0;
-
-    for (int i = 0; i < M * N; i++)
-    {
-        float diff = fabs(hC[i] - hC_cpu[i]);
-
-        maxError = std::max(maxError, diff);
-
-        if (diff > 1e-3f)
-            badCount++;
-    }
-
-    std::cout << "Max error: " << maxError << std::endl;
-    std::cout << "Bad elements: " << badCount << " / " << (M * N) << std::endl;    
-
-    return 0;
+    cudaFree(dC);
+    delete[] hC;
 }
+
+
+//
+// int main()
+// {
+//     Vb::Timer gpuClock;
+//     int M = 2500;
+//     int N = 2505;
+//     int K = 2502;
+//
+//     int pM= Vb::GetPadding(M, 4);
+//     int pN= Vb::GetPadding(N, 4);
+//     int pK= Vb::GetPadding(K, 4);
+//
+//     size_t sizeA = M * K;
+//     size_t sizeB = K * N;
+//     size_t sizeC = M * N;
+//
+//     float* hA = new float[sizeA];
+//     float* hB = new float[sizeB];
+//     float* hC = new float[sizeC];
+//
+//     for (size_t i = 0; i < sizeA; i++) hA[i] = 1.0f;
+//     for (size_t i = 0; i < sizeB; i++) hB[i] = 2.0f;
+//
+//     Vb::Variable<float> dA;
+//     Vb::Variable<float> dB;
+//     Vb::Variable<float> dC;
+//
+//     dA.Allocate(pM * pK);
+//     dB.Allocate(pK * pN);
+//     dC.Allocate(pM * pN);
+//
+//     dA.Write(hA, sizeA);
+//     dB.Write(hB, sizeB);
+//
+//     gpuClock.StartRecord();
+//
+//     Vb::GEMM(dA.Get(), dB.Get(), dC.Get(), M, N, K);
+//     CUDA_CHECK("An error occured in GEMM");
+//     cudaDeviceSynchronize();
+//
+//     gpuClock.StopRecord();
+//
+//     dC.Read(hC, sizeC);
+//
+//     std::cout << "GPU Time: " << gpuClock.GetTime() << "\n";
+//     std::cout << "M_padded=" << M << " N_padded=" << N << " K_padded=" << K << "\n";
+//     for (int i = 0; i < 10; i++)
+//         std::cout << hC[i] << " ";
+//     std::cout << "\n expected =" << 2.0f * K;
+//
+//     float* hC_cpu = new float[sizeC];
+//     cpuGEMM(hA, hB, hC_cpu, M, N, K);
+//
+//     float maxError = 0.0f;
+//     int badCount = 0;
+//
+//     for (int i = 0; i < M * N; i++)
+//     {
+//         float diff = fabs(hC[i] - hC_cpu[i]);
+//
+//         maxError = std::max(maxError, diff);
+//
+//         if (diff > 1e-3f)
+//             badCount++;
+//     }
+//
+//     std::cout << "Max error: " << maxError << std::endl;
+//     std::cout << "Bad elements: " << badCount << " / " << (M * N) << std::endl;    
+//
+//     return 0;
+// }
 
